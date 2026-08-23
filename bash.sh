8f822cfeb0226803fbe8318033e64ad0767f39f6
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-23 12:58:13 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,6 @@
+curl -X POST 'https://iam.cloud.ibm.com/identity/token' \
+-H 'Content-Type: application/x-www-form-urlencoded' \
+-d 'grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=MY_APIKEY'
 pacman -S utilities
 pacman -S vim-plugins
 pacman -S editorconfig-vim
